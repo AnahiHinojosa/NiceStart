30a0e4f1a7ac575f49cfcd622d95791636c9b2f3
anahi hinojosa <ana11hinograje@gmail.com>
2024-12-18 13:51:30 +0100

    string lenguas

--- app/src/main/java/com/ahinojosa/nicestart/Login.java
+++ app/src/main/java/com/ahinojosa/nicestart/Login.java
@@ -1,10 +1,11 @@
 package com.ahinojosa.nicestart;
 
+import static androidx.core.content.ContextCompat.startActivity;
+
+
 import android.content.Intent;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,8 +13,6 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 public class Login extends AppCompatActivity {
 
     @Override
@@ -23,7 +22,7 @@ public class Login extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
 
-/*
+/*¡
         ImageView mGirl = findViewById(R.id.girl);
 
         Glide.with(this)
@@ -32,12 +31,12 @@ public class Login extends AppCompatActivity {
                                 .centerCrop()
                                         .placeholder(new ColorDrawable(this.getResources().getColor(R.color.rosa_pas)))
                                                 .into(mGirl);
-
+*/
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });*/
+        });
     }
 
     public void openSignup(View view) {
@@ -49,4 +48,5 @@ public class Login extends AppCompatActivity {
         Intent intent = new Intent(Login.this, Main.class);
         startActivity(intent);
     }
+
 }
